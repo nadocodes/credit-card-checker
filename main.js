6a9c2a4e94b6becf21033a34156abeb49d5539a2
nadocodes <naderantar96@gmail.com>
2023-01-25 16:44:39 +0000

    comments added to define specific functions, tests added

--- main.js
+++ main.js
@@ -1,29 +1,46 @@
 // All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
 // All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
 // Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
-
+const batch = [
+  valid1,
+  valid2,
+  valid3,
+  valid4,
+  valid5,
+  invalid1,
+  invalid2,
+  invalid3,
+  invalid4,
+  invalid5,
+  mystery1,
+  mystery2,
+  mystery3,
+  mystery4,
+  mystery5,
+];
 
 // Add your functions below:
+
+// Luhn Algorithm to check if a credit card number is valid or not
 const validateCred = (arr) => {
   let sum = 0;
   for (let i = arr.length - 1; i >= 0; i--) {
@@ -39,6 +56,7 @@ const validateCred = (arr) => {
   return sum % 10 === 0;
 };
 
+// Find all invalid credit card numbers
 const findInvalidCards = (arr) => {
   let invalidCards = [];
   for (let i = 0; i < arr.length; i++) {
@@ -49,39 +67,42 @@ const findInvalidCards = (arr) => {
   return invalidCards;
 };
 
+// Find the companies that have issued invalid credit card numbers
 const idInvalidCardCompanies = (arr) => {
   let companies = [];
   for (let i = 0; i < arr.length; i++) {
     switch (arr[i][0]) {
       case 3:
-        if (companies.indexOf('Amex') === -1) {
-          companies.push('Amex');
+        if (companies.indexOf("Amex") === -1) {
+          companies.push("Amex");
         }
         break;
       case 4:
-        if (companies.indexOf('Visa') === -1) {
-          companies.push('Visa');
+        if (companies.indexOf("Visa") === -1) {
+          companies.push("Visa");
         }
         break;
       case 5:
-        if (companies.indexOf('Mastercard') === -1) {
-          companies.push('Mastercard');
+        if (companies.indexOf("Mastercard") === -1) {
+          companies.push("Mastercard");
         }
         break;
       case 6:
-        if (companies.indexOf('Discover') === -1) {
-          companies.push('Discover');
+        if (companies.indexOf("Discover") === -1) {
+          companies.push("Discover");
         }
         break;
       default:
-        console.log('Company not found');
+        console.log("Company not found");
     }
   }
   return companies;
 };
 
-
-
-
-
-
+// Test
+console.log(validateCred(valid1));
+console.log(validateCred(valid5));
+console.log(validateCred(invalid2));
+console.log(validateCred(mystery3));
+console.log(findInvalidCards(batch));
+console.log(idInvalidCardCompanies(findInvalidCards(batch)).join(", "));
